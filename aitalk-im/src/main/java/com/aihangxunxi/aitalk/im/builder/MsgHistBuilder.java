339eed840fd9e89497e96bc74fbcfef302b68ddb
陈庆泽 <chenqingze107@163.com>
2020-09-15 10:22:03 +0800

    add builder for proto vs model

--- aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/builder/MsgHistBuilder.java
+++ aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/builder/MsgHistBuilder.java
@@ -2,11 +2,13 @@ package com.aihangxunxi.aitalk.im.builder;
 
 import com.aihangxunxi.aitalk.im.protocol.buffers.Message;
 import com.aihangxunxi.aitalk.storage.model.BaseModel;
+import org.springframework.stereotype.Component;
 
 /**
  * @author chenqingze107@163.com
  * @version 1.0
  */
+@Component
 public class MsgHistBuilder implements BaseBuilder {
 
 	@Override
