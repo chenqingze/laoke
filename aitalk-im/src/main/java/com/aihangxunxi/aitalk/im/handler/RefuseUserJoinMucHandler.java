caa565ad9e6590a5ab684598ed5aee73586440d5
王泓超 <wang@wangdeMac-mini.local>
2020-10-13 17:56:08 +0800

    m

--- aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/handler/RefuseUserJoinMucHandler.java
+++ aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/handler/RefuseUserJoinMucHandler.java
@@ -18,45 +18,36 @@ import javax.annotation.Resource;
 @ChannelHandler.Sharable
 public class RefuseUserJoinMucHandler extends ChannelInboundHandlerAdapter {
 
-    @Resource
-    private InvitationRepository invitationRepository;
+	@Resource
+	private InvitationRepository invitationRepository;
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (msg instanceof Message && ((Message) msg).getOpCode() == OpCode.REFUSE_USER_JOIN_MUC_REQUEST) {
-            String groupId = ((Message) msg).getRefuseUserJoinMucRequest().getGroupId();
-            String userId = ((Message) msg).getRefuseUserJoinMucRequest().getUserId();
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		if (msg instanceof Message && ((Message) msg).getOpCode() == OpCode.REFUSE_USER_JOIN_MUC_REQUEST) {
+			String groupId = ((Message) msg).getRefuseUserJoinMucRequest().getGroupId();
+			String userId = ((Message) msg).getRefuseUserJoinMucRequest().getUserId();
 
-            if (invitationRepository.declinedGroupInvitation(groupId, Long.parseLong(userId))) {
-                Message ack = Message.newBuilder()
-                        .setSeq(((Message) msg).getSeq())
-                        .setOpCode(OpCode.REFUSE_USER_JOIN_MUC_ACK)
-                        .setRefuseUserJoinMucAck(RefuseUserJoinMucAck.newBuilder()
-                                .setGroupId(groupId)
-                                .setUserId(userId)
-                                .setSuccess("ok")
-                                .setMessage("已拒绝此用户加入群")
-                                .build())
-                        .build();
-                ctx.writeAndFlush(ack);
-            } else {
-                Message ack = Message.newBuilder()
-                        .setSeq(((Message) msg).getSeq())
-                        .setOpCode(OpCode.REFUSE_USER_JOIN_MUC_ACK)
-                        .setRefuseUserJoinMucAck(RefuseUserJoinMucAck.newBuilder()
-                                .setGroupId(groupId)
-                                .setUserId(userId)
-                                .setSuccess("no")
-                                .setMessage("拒绝失败，请稍后再试")
-                                .build())
-                        .build();
-                ctx.writeAndFlush(ack);
-            }
+			if (invitationRepository.declinedGroupInvitation(groupId, Long.parseLong(userId))) {
+				Message ack = Message.newBuilder().setSeq(((Message) msg).getSeq())
+						.setOpCode(OpCode.REFUSE_USER_JOIN_MUC_ACK)
+						.setRefuseUserJoinMucAck(RefuseUserJoinMucAck.newBuilder().setGroupId(groupId).setUserId(userId)
+								.setSuccess("ok").setMessage("已拒绝此用户加入群").build())
+						.build();
+				ctx.writeAndFlush(ack);
+			}
+			else {
+				Message ack = Message.newBuilder().setSeq(((Message) msg).getSeq())
+						.setOpCode(OpCode.REFUSE_USER_JOIN_MUC_ACK)
+						.setRefuseUserJoinMucAck(RefuseUserJoinMucAck.newBuilder().setGroupId(groupId).setUserId(userId)
+								.setSuccess("no").setMessage("拒绝失败，请稍后再试").build())
+						.build();
+				ctx.writeAndFlush(ack);
+			}
 
-
-        } else {
-            ctx.fireChannelRead(msg);
-        }
-    }
+		}
+		else {
+			ctx.fireChannelRead(msg);
+		}
+	}
 
 }
--- aitalk-storage/src/main/java/com/aihangxunxi/aitalk/storage/repository/InvitationRepository.java
+++ aitalk-storage/src/main/java/com/aihangxunxi/aitalk/storage/repository/InvitationRepository.java
@@ -25,53 +25,52 @@ import static com.mongodb.client.model.Updates.set;
 @Repository
 public class InvitationRepository {
 
-    @Resource
-    private MongoDatabase db;
+	@Resource
+	private MongoDatabase db;
 
-    public boolean save(Invitation invitation) {
-        InsertOneResult result = db.getCollection("invitation", Invitation.class).insertOne(invitation);
-        return result.wasAcknowledged();
-    }
+	public boolean save(Invitation invitation) {
+		InsertOneResult result = db.getCollection("invitation", Invitation.class).insertOne(invitation);
+		return result.wasAcknowledged();
+	}
 
-    public Invitation updateInviteStatus(String id, String inviteStatus) {
-        Invitation invitation = db.getCollection("invitation", Invitation.class).findOneAndUpdate(
-                eq("_id", new ObjectId(id)),
-                combine(set("inviteStatus", inviteStatus), set("updatedAt", System.currentTimeMillis())));
-        return invitation;
-    }
+	public Invitation updateInviteStatus(String id, String inviteStatus) {
+		Invitation invitation = db.getCollection("invitation", Invitation.class).findOneAndUpdate(
+				eq("_id", new ObjectId(id)),
+				combine(set("inviteStatus", inviteStatus), set("updatedAt", System.currentTimeMillis())));
+		return invitation;
+	}
 
-    // 保存群邀请
-    public boolean saveGroupInvitation(Invitation invitation) {
-        InsertOneResult result = db.getCollection("invitation", Invitation.class).insertOne(invitation);
-        return result.wasAcknowledged();
-    }
+	// 保存群邀请
+	public boolean saveGroupInvitation(Invitation invitation) {
+		InsertOneResult result = db.getCollection("invitation", Invitation.class).insertOne(invitation);
+		return result.wasAcknowledged();
+	}
 
-    // 更新邀请状态为已邀请
-    public boolean updateGroupInvitationStatus(String groupId, Long userId) {
-        MongoCollection<Invitation> invitationMongoCollection = db.getCollection("invitation", Invitation.class);
-        Bson bson = and(eq("addresseeId", groupId), eq("requesterId", userId));
-        Bson bson1 = set("inviteStatus", InviteStatus.ACCEPTED);
-        invitationMongoCollection.updateOne(bson, bson1);
-        return true;
-    }
+	// 更新邀请状态为已邀请
+	public boolean updateGroupInvitationStatus(String groupId, Long userId) {
+		MongoCollection<Invitation> invitationMongoCollection = db.getCollection("invitation", Invitation.class);
+		Bson bson = and(eq("addresseeId", groupId), eq("requesterId", userId));
+		Bson bson1 = set("inviteStatus", InviteStatus.ACCEPTED);
+		invitationMongoCollection.updateOne(bson, bson1);
+		return true;
+	}
 
-    // 更新状态为忽略
-    public boolean ignoredGroupInvitation(String groupId, Long userId) {
-        MongoCollection<Invitation> invitationMongoCollection = db.getCollection("invitation", Invitation.class);
-        Bson bson = and(eq("addresseeId", groupId), eq("requesterId", userId));
-        Bson bson1 = set("inviteStatus", InviteStatus.IGNORED);
-        invitationMongoCollection.updateOne(bson, bson1);
-        return true;
-    }
+	// 更新状态为忽略
+	public boolean ignoredGroupInvitation(String groupId, Long userId) {
+		MongoCollection<Invitation> invitationMongoCollection = db.getCollection("invitation", Invitation.class);
+		Bson bson = and(eq("addresseeId", groupId), eq("requesterId", userId));
+		Bson bson1 = set("inviteStatus", InviteStatus.IGNORED);
+		invitationMongoCollection.updateOne(bson, bson1);
+		return true;
+	}
 
-    // 拒绝用户加入群
-    public boolean declinedGroupInvitation(String groupId, Long userId) {
-        MongoCollection<Invitation> invitationGroupMongoCollection = db.getCollection("invitation", Invitation.class);
-        Bson bson = and(eq("requesterId", userId), eq("addresseeId", groupId));
-        Bson bson1 = set("inviteStatus", InviteStatus.DECLINED);
-        invitationGroupMongoCollection.updateOne(bson, bson1);
-        return true;
-    }
-
+	// 拒绝用户加入群
+	public boolean declinedGroupInvitation(String groupId, Long userId) {
+		MongoCollection<Invitation> invitationGroupMongoCollection = db.getCollection("invitation", Invitation.class);
+		Bson bson = and(eq("requesterId", userId), eq("addresseeId", groupId));
+		Bson bson1 = set("inviteStatus", InviteStatus.DECLINED);
+		invitationGroupMongoCollection.updateOne(bson, bson1);
+		return true;
+	}
 
 }
