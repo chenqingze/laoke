9eade9e3bb57c7c785e70e7b89a681964aa6c067
王泓超 <wang@wangdeMac-mini.local>
2020-11-03 13:27:41 +0800

    bind相关

--- aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/handler/BindUserDeviceHandler.java
+++ aitalk-im/src/main/java/com/aihangxunxi/aitalk/im/handler/BindUserDeviceHandler.java
@@ -17,23 +17,25 @@ import javax.annotation.Resource;
 @ChannelHandler.Sharable
 public class BindUserDeviceHandler extends ChannelInboundHandlerAdapter {
 
-    @Resource
-    private UserRepository userRepository;
+	@Resource
+	private UserRepository userRepository;
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (msg instanceof Message && ((Message) msg).getOpCode() == OpCode.BIND_USER_DEVICE_REQUEST) {
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		if (msg instanceof Message && ((Message) msg).getOpCode() == OpCode.BIND_USER_DEVICE_REQUEST) {
 
-            String userId = ctx.channel().attr(ChannelConstant.USER_ID_ATTRIBUTE_KEY).get();
+			String userId = ctx.channel().attr(ChannelConstant.USER_ID_ATTRIBUTE_KEY).get();
 
-            String deviceCode = ((Message) msg).getBindUserDeviceRequest().getDeviceCode();
-            String deviceType = ((Message) msg).getBindUserDeviceRequest().getDeviceType();
-            String deviceIdiom = ((Message) msg).getBindUserDeviceRequest().getDeviceIdiom();
+			String deviceCode = ((Message) msg).getBindUserDeviceRequest().getDeviceCode();
+			String deviceType = ((Message) msg).getBindUserDeviceRequest().getDeviceType();
+			String deviceIdiom = ((Message) msg).getBindUserDeviceRequest().getDeviceIdiom();
 
-            User user = userRepository.getUserById(new ObjectId(userId));
-            this.userRepository.updateDeviceInfo(user.getUserId(), deviceCode, deviceType);
-        } else {
-            ctx.fireChannelRead(msg);
-        }
-    }
+			User user = userRepository.getUserById(new ObjectId(userId));
+			this.userRepository.updateDeviceInfo(user.getUserId(), deviceCode, deviceType);
+		}
+		else {
+			ctx.fireChannelRead(msg);
+		}
+	}
+
 }
